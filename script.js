8f2ee53287e976768cdaae01a41eb1ddb4663ea3
vishal <Vishalgehlot334@gmail.com>
2023-02-24 15:07:10 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const openMenu=()=>{
+    if(document.getElementById('opnmen').style.display=='none'){
+        document.getElementById('opnmen').style.display='flex'
+    }
+    else{
+        document.getElementById('opnmen').style.display='none'
+}
+}
+
